b787cf900fc4c4fad75000d2593dd5791f671208
Alex <alexphp47@gmail.com>
2020-05-10 20:04:01 -0400

    third commit

--- app/src/main/java/com/example/try6/MainActivity.kt
+++ app/src/main/java/com/example/try6/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-// second commit
+// third commit
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
