9276781cf30ba0b3802247abf8f3fc09388e08ea
Alex <alexphp47@gmail.com>
2020-05-10 20:00:29 -0400

    first commit

---
+++ app/src/main/java/com/example/try6/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.try6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+// first commit
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
